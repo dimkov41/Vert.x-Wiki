a4c3e750d1a50bc7112eab1ec3239e12e21c30ee
Radostin Dimkov <radostin.dimkov@gravis.bg>
2019-12-13 15:09:09 +0200

    Prevent eating exceptions

--- src/main/java/io/vertx/starter/database/SqlLoader.java
+++ src/main/java/io/vertx/starter/database/SqlLoader.java
@@ -4,6 +4,8 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.starter.database.enums.SqlQuery;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,31 +14,28 @@ import java.util.Properties;
 public class SqlLoader {
   private static final String CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE = "wikidb.sqlqueries.resource.file";
 
-  public static Map<SqlQuery,String> loadSqlQueries(JsonObject config) {
+  public static Map<SqlQuery,String> loadSqlQueries(JsonObject config) throws IOException {
     Map<SqlQuery, String> sqlQueries = null;
-    try {
-      String queriesFile = config.getString(CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE);
-      InputStream queriesInputStream;
-      if (queriesFile != null) {
-        queriesInputStream = new FileInputStream(queriesFile);
-      } else {
-        queriesInputStream = SqlLoader.class.getResourceAsStream("/db-queries.properties");
-      }
-
-      Properties queriesProps = new Properties();
-      queriesProps.load(queriesInputStream);
-      queriesInputStream.close();
-
-      sqlQueries = new HashMap<>();
-      sqlQueries.put(SqlQuery.CREATE_PAGES_TABLE, queriesProps.getProperty("create-pages-table"));
-      sqlQueries.put(SqlQuery.ALL_PAGES, queriesProps.getProperty("all-pages"));
-      sqlQueries.put(SqlQuery.GET_PAGE, queriesProps.getProperty("get-page"));
-      sqlQueries.put(SqlQuery.CREATE_PAGE, queriesProps.getProperty("create-page"));
-      sqlQueries.put(SqlQuery.SAVE_PAGE, queriesProps.getProperty("save-page"));
-      sqlQueries.put(SqlQuery.DELETE_PAGE, queriesProps.getProperty("delete-page"));
-    } catch (Exception e) {
-      System.err.println("Exception occured when loading sql queries: " + e.getMessage());
+    String queriesFile = config.getString(CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE);
+    InputStream queriesInputStream;
+    if (queriesFile != null) {
+      queriesInputStream = new FileInputStream(queriesFile);
+    } else {
+      queriesInputStream = SqlLoader.class.getResourceAsStream("/db-queries.properties");
     }
+
+    Properties queriesProps = new Properties();
+    queriesProps.load(queriesInputStream);
+    queriesInputStream.close();
+
+    sqlQueries = new HashMap<>();
+    sqlQueries.put(SqlQuery.CREATE_PAGES_TABLE, queriesProps.getProperty("create-pages-table"));
+    sqlQueries.put(SqlQuery.ALL_PAGES, queriesProps.getProperty("all-pages"));
+    sqlQueries.put(SqlQuery.GET_PAGE, queriesProps.getProperty("get-page"));
+    sqlQueries.put(SqlQuery.CREATE_PAGE, queriesProps.getProperty("create-page"));
+    sqlQueries.put(SqlQuery.SAVE_PAGE, queriesProps.getProperty("save-page"));
+    sqlQueries.put(SqlQuery.DELETE_PAGE, queriesProps.getProperty("delete-page"));
+
     return sqlQueries;
   }
 }
